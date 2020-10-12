149090101cce3afb4a5b8a4a7a9e58cc5f8cc8cf
Stephan Ringel <ringel@adobe.com>
2020-10-12 13:51:16 +0200

    fix: Update Adobe logo color

--- packages/gatsby-theme-parliament/src/components/Icons/index.js
+++ packages/gatsby-theme-parliament/src/components/Icons/index.js
@@ -22,7 +22,7 @@ const commonProps = {
 
 // Used by GlobalNav
 export const Adobe = (props) => (
-  <svg {...props} {...commonProps} viewBox="0 0 30 26" fill="#E1251B" aria-label="Adobe">
+  <svg {...props} {...commonProps} viewBox="0 0 30 26" fill="#FA0F00" aria-label="Adobe">
     <polygon points="19,0 30,0 30,26"></polygon>
     <polygon points="11.1,0 0,0 0,26"></polygon>
     <polygon points="15,9.6 22.1,26 17.5,26 15.4,20.8 10.2,20.8"></polygon>
