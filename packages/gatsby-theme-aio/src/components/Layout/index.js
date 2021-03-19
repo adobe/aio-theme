873200905e78895d5b39a2c82192e6df16d33a77
Stephan Ringel <ringel@adobe.com>
2021-03-19 10:17:34 +0100

    fix: Images and layout are broken when JS is disabled

--- packages/gatsby-theme-aio/src/components/Layout/index.js
+++ packages/gatsby-theme-aio/src/components/Layout/index.js
@@ -11,6 +11,7 @@
  */
 
 import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
 import { css } from '@emotion/react';
 import loadable from '@loadable/component';
 import { useStaticQuery, graphql } from 'gatsby';
@@ -29,6 +30,7 @@ import { GlobalHeader } from '../GlobalHeader';
 import { GlobalHeaderTemp } from '../GlobalHeader/temp';
 import { SEO } from '../SEO';
 import { ProgressCircle } from '../ProgressCircle';
+import nextId from 'react-id-generator';
 
 // Cache spec
 let openAPISpec;
@@ -161,159 +163,238 @@ export default ({ children, pageContext, location }) => {
     });
   }
 
-  return (
-    <Provider
-      value={{
-        location,
-        pageContext,
-        hasSideNav,
-        siteMetadata,
-        pathPrefix,
-        allSitePage,
-        allMdx,
-        allGithub,
-        allGithubContributors
-      }}>
-      <SEO title={frontMatter?.title} description={frontMatter?.description} />
-      <div
-        dir="ltr"
-        className="spectrum spectrum--medium spectrum--large spectrum--light"
-        color-scheme="light"
-        css={css`
-          min-height: 100vh;
-          background-color: var(--spectrum-global-color-gray-50);
-        `}>
-        {hasLayout ? (
-          <>
-            <div
-              css={css`
-                display: grid;
-                grid-template-areas: 'header header' 'sidenav main';
-                grid-template-rows: var(--spectrum-global-dimension-size-800);
-                grid-template-columns: ${hasSideNav ? '256px auto' : '0 auto'};
+  const layoutId = nextId();
+  const sideNavId = nextId();
 
-                ${hasLayout &&
-                `@media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
-                  grid-template-columns: 0px auto;
-                }`}
-              `}>
+  return (
+    <>
+      <Helmet>
+        <style>
+          {`
+          @font-face {
+            font-family: "adobe-clean";
+            src: url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"), url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"), url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+            font-display: swap;
+            font-style: normal;
+            font-weight: 400;
+          }
+          
+          @font-face {
+            font-family: "adobe-clean";
+            src: url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("woff2"), url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("woff"), url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("opentype");
+            font-display: swap;
+            font-style: italic;
+            font-weight: 400;
+          }
+          
+          @font-face {
+            font-family: "adobe-clean";
+            src: url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+            font-display: swap;
+            font-style: normal;
+            font-weight: 700;
+          }
+          
+          @font-face {
+            font-family: "adobe-clean";
+            src: url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff2"), url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff"), url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("opentype");
+            font-display: swap;
+            font-style: normal;
+            font-weight: 300;
+          }
+          
+          @font-face {
+            font-family: "adobe-clean-serif";
+            src: url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("woff2"), url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("woff"), url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("opentype");
+            font-display: swap;
+            font-style: normal;
+            font-weight: 900;
+          }
+          
+          html, body {
+            margin: 0;
+            text-size-adjust: none;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+          
+          *[hidden] {
+            display: none !important;
+          }`}
+        </style>
+        <noscript>
+          {`
+          <style>
+            #${layoutId} {
+              grid-template-columns: 0 auto;
+            }
+            
+            #${sideNavId} {
+              display: none !important;
+            }
+            
+            .gatsby-resp-image-image {
+              opacity: 1 !important;
+            }
+          </style>
+          `}
+        </noscript>
+      </Helmet>
+      <Provider
+        value={{
+          location,
+          pageContext,
+          hasSideNav,
+          siteMetadata,
+          pathPrefix,
+          allSitePage,
+          allMdx,
+          allGithub,
+          allGithubContributors
+        }}>
+        <SEO title={frontMatter?.title} description={frontMatter?.description} />
+        <div
+          dir="ltr"
+          className="spectrum spectrum--medium spectrum--large spectrum--light"
+          color-scheme="light"
+          css={css`
+            min-height: 100vh;
+            background-color: var(--spectrum-global-color-gray-50);
+          `}>
+          {hasLayout ? (
+            <>
               <div
+                id={layoutId}
                 css={css`
-                  grid-area: header;
-                  position: fixed;
-                  height: ${hasGlobalHeaderTemp
-                    ? 'var(--spectrum-global-dimension-size-1000)'
-                    : 'var(--spectrum-global-dimension-size-800)'};
-                  left: 0;
-                  right: 0;
-                  background-color: var(--spectrum-global-color-gray-50);
-                  z-index: 2;
-                `}>
-                {hasGlobalHeaderTemp ? (
-                  <GlobalHeaderTemp />
-                ) : (
-                  <GlobalHeader
-                    menu={menu}
-                    versions={versions}
-                    pages={pages}
-                    docs={docs}
-                    location={location}
-                    hasSideNav={hasSideNav}
-                    toggleSideNav={() => {
-                      toggleSideNav(setShowSideNav);
-                    }}
-                  />
-                )}
-              </div>
-              <div
-                hidden={!hasSideNav}
-                css={css`
-                  grid-area: sidenav;
-                  position: fixed;
-                  z-index: 1;
-                  width: 256px;
-                  height: 100%;
-                  background-color: var(--spectrum-global-color-gray-75);
+                  display: grid;
+                  grid-template-areas: 'header header' 'sidenav main';
+                  grid-template-rows: var(--spectrum-global-dimension-size-800);
+                  grid-template-columns: ${hasSideNav ? '256px auto' : '0 auto'};
 
                   ${hasLayout &&
                   `@media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
-                    transition: transform var(--spectrum-global-animation-duration-200) ease-in-out;
-                    transform: translateX(${showSideNav ? '0' : '-100%'});
+                    grid-template-columns: 0px auto;
                   }`}
                 `}>
-                {SideNav && (
-                  <SideNav
-                    selectedPages={selectedPages}
-                    selectedSubPages={selectedSubPages}
-                    searchIndex={ParliamentSearchIndex}
-                  />
-                )}
-              </div>
-              <div
-                css={css`
-                  grid-area: main;
-                `}>
-                <div hidden={!hasOpenAPISpec}>
-                  {openAPISpec && (
-                    <main
-                      css={css`
-                        [role='navigation'] [role='menuitem'] + ul {
-                          display: block;
-                        }
-                      `}>
-                      {OpenAPIBlock && <OpenAPIBlock specUrl={openAPISpec} />}
-                    </main>
+                <div
+                  css={css`
+                    grid-area: header;
+                    position: fixed;
+                    height: ${hasGlobalHeaderTemp
+                      ? 'var(--spectrum-global-dimension-size-1000)'
+                      : 'var(--spectrum-global-dimension-size-800)'};
+                    left: 0;
+                    right: 0;
+                    background-color: var(--spectrum-global-color-gray-50);
+                    z-index: 2;
+                  `}>
+                  {hasGlobalHeaderTemp ? (
+                    <GlobalHeaderTemp />
+                  ) : (
+                    <GlobalHeader
+                      menu={menu}
+                      versions={versions}
+                      pages={pages}
+                      docs={docs}
+                      location={location}
+                      hasSideNav={hasSideNav}
+                      toggleSideNav={() => {
+                        toggleSideNav(setShowSideNav);
+                      }}
+                    />
                   )}
                 </div>
-                {!hasOpenAPISpec && children}
+                <div
+                  id={sideNavId}
+                  hidden={!hasSideNav}
+                  css={css`
+                    grid-area: sidenav;
+                    position: fixed;
+                    z-index: 1;
+                    width: 256px;
+                    height: 100%;
+                    background-color: var(--spectrum-global-color-gray-75);
+
+                    ${hasLayout &&
+                    `@media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
+                      transition: transform var(--spectrum-global-animation-duration-200) ease-in-out;
+                      transform: translateX(${showSideNav ? '0' : '-100%'});
+                    }`}
+                  `}>
+                  {SideNav && (
+                    <SideNav
+                      selectedPages={selectedPages}
+                      selectedSubPages={selectedSubPages}
+                      searchIndex={ParliamentSearchIndex}
+                    />
+                  )}
+                </div>
+                <div
+                  css={css`
+                    grid-area: main;
+                  `}>
+                  <div hidden={!hasOpenAPISpec}>
+                    {openAPISpec && (
+                      <main
+                        css={css`
+                          [role='navigation'] [role='menuitem'] + ul {
+                            display: block;
+                          }
+                        `}>
+                        {OpenAPIBlock && <OpenAPIBlock specUrl={openAPISpec} />}
+                      </main>
+                    )}
+                  </div>
+                  {!hasOpenAPISpec && children}
+                </div>
               </div>
-            </div>
 
-            <div
-              css={css`
-                position: fixed;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                display: ${isLoading ? 'grid' : 'none'};
-                place-items: center center;
-              `}>
-              <ProgressCircle />
-            </div>
-
-            <div
-              css={css`
-                display: none;
-
-                @media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
-                  display: block;
-                  transition: opacity 160ms ease-in;
-                  background-color: rgba(0, 0, 0, 0.4);
-                  pointer-events: none;
-                  opacity: 0;
+              <div
+                css={css`
                   position: fixed;
                   top: 0;
                   left: 0;
-                  height: 100%;
-                  width: 100%;
+                  right: 0;
+                  bottom: 0;
+                  display: ${isLoading ? 'grid' : 'none'};
+                  place-items: center center;
+                `}>
+                <ProgressCircle />
+              </div>
 
-                  ${showSideNav &&
-                  `
+              <div
+                css={css`
+                  display: none;
+
+                  @media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
+                    display: block;
+                    transition: opacity 160ms ease-in;
+                    background-color: rgba(0, 0, 0, 0.4);
+                    pointer-events: none;
+                    opacity: 0;
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    height: 100%;
+                    width: 100%;
+
+                    ${showSideNav &&
+                    `
                     pointer-events: auto;
                     opacity: 1;
                   `}
-                }
-              `}
-              onClick={() => {
-                toggleSideNav(setShowSideNav);
-              }}
-            />
-          </>
-        ) : (
-          children
-        )}
-      </div>
-    </Provider>
+                  }
+                `}
+                onClick={() => {
+                  toggleSideNav(setShowSideNav);
+                }}
+              />
+            </>
+          ) : (
+            children
+          )}
+        </div>
+      </Provider>
+    </>
   );
 };
--- packages/gatsby-theme-aio/src/components/SEO/index.js
+++ packages/gatsby-theme-aio/src/components/SEO/index.js
@@ -22,59 +22,6 @@ const SEO = ({ title = 'Adobe I/O', description = 'Adobe I/O Site' }) => (
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     <link rel="icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
-
-    <style>{`
-        @font-face {
-          font-family: "adobe-clean";
-          src: url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"), url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"), url("https://use.typekit.net/af/cb695f/000000000000000000017701/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
-          font-display: swap;
-          font-style: normal;
-          font-weight: 400;
-        }
-        
-        @font-face {
-          font-family: "adobe-clean";
-          src: url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("woff2"), url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("woff"), url("https://use.typekit.net/af/74ffb1/000000000000000000017702/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3") format("opentype");
-          font-display: swap;
-          font-style: italic;
-          font-weight: 400;
-        }
-        
-        @font-face {
-          font-family: "adobe-clean";
-          src: url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/eaf09c/000000000000000000017703/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-          font-display: swap;
-          font-style: normal;
-          font-weight: 700;
-        }
-        
-        @font-face {
-          font-family: "adobe-clean";
-          src: url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff2"), url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff"), url("https://use.typekit.net/af/40207f/0000000000000000000176ff/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("opentype");
-          font-display: swap;
-          font-style: normal;
-          font-weight: 300;
-        }
-        
-        @font-face {
-          font-family: "adobe-clean-serif";
-          src: url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("woff2"), url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("woff"), url("https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3") format("opentype");
-          font-display: swap;
-          font-style: normal;
-          font-weight: 900;
-        }
-        
-        html, body {
-          margin: 0;
-          text-size-adjust: none;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        }
-        
-        *[hidden] {
-          display: none !important;
-        }
-      `}</style>
   </Helmet>
 );
 
