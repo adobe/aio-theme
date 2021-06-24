7ebbd330c90b480bdb73c5dc2aeaf294713fa5e0
Valerii Naida <vnayda@adobe.com>
2021-06-24 10:26:03 -0500

    feat(DEVSITE-91, DEVSITE-93): add support of "frame" directive / Add support of "OpenAPI" directive

---
+++ packages/gatsby-theme-aio/algolia/records/create-records-for-frame.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+const AlgoliaHTMLExtractor = require('algolia-html-extractor');
+const fs = require('fs');
+const normalizePath = require('normalize-path');
+
+/**
+ * Support of "frameSrc" directive:
+ * https://github.com/adobe/gatsby-theme-aio#frame
+ */
+class CreateRecordsForFrame {
+  constructor(options = {}) {
+    this.htmlExtractor = new AlgoliaHTMLExtractor();
+  }
+
+  /**
+   * @param {Object} node
+   * @param {Object} options
+   * @return {Array}
+   */
+  execute(node, options) {
+    const { fileAbsolutePath, ...restNodeFields } = node;
+    const [siteDirAbsolutePath] = normalizePath(fileAbsolutePath).split(options.pagesSourceDir);
+    const staticFileAbsolutePath = `${siteDirAbsolutePath}${options.staticSourceDir}${node.frameSrc}`;
+
+    if (!fs.existsSync(staticFileAbsolutePath)) {
+      throw Error(`Static file resolving error: no such file "${staticFileAbsolutePath}"`);
+    }
+
+    const fileContent = fs.readFileSync(staticFileAbsolutePath, 'utf8');
+
+    const extractedData = this.htmlExtractor
+      .run(fileContent, { cssSelector: options.tagsToIndex })
+      .filter((htmlTag) => htmlTag.content.length >= options.minCharsLengthPerTag);
+
+    delete restNodeFields.mdxAST;
+    delete restNodeFields.fileAbsolutePath;
+    delete restNodeFields.frameSrc;
+    return extractedData.map((htmlTag) => ({
+      ...restNodeFields,
+      objectID: htmlTag.objectID,
+      content: htmlTag.content,
+      headings: htmlTag.headings,
+      customRanking: htmlTag.customRanking,
+      internalObjectID: node.objectID
+    }));
+  }
+}
+module.exports = CreateRecordsForFrame;
