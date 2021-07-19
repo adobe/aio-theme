7ba668d10a4e132d4eb6e13aa8bf43f2f64b793e
Bruce Denham <bdenham@adobe.com>
2021-07-19 08:01:03 -0500

    feat(search): wip-ui search work

--- packages/gatsby-theme-aio/src/components/SearchPage/index.js
+++ packages/gatsby-theme-aio/src/components/SearchPage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Tabs, Item as TabsItem, Label as TabsItemLabel } from '../Tabs';
+
 import {
   InstantSearch,
   Panel,
@@ -13,32 +15,37 @@ import {
   Configure,
   connectStateResults
 } from 'react-instantsearch-dom';
+
 import withURLSync from './URLSync';
 import algoliasearch from 'algoliasearch/lite';
 import { NoResults } from '../SearchWidgets';
+
 import { css } from '@emotion/react';
 import PropTypes from 'prop-types';
 
-import './test.css';
+import 'instantsearch.css/themes/satellite.css';
 import './index.css';
 
 const searchClient = algoliasearch('E642SEDTHL', '36561fc0f6d8f1ecf996bc7bf41af00f');
 
 const Hit = ({ hit }) => {
   return (
-    <article className="hit">
-      <h1 className="hit-title">
+    <div>
+      <div className="hit-title">
         <Highlight attribute="contentHeading" hit={hit} />
-      </h1>
+      </div>
       <p className="hit-description">
         <Highlight attribute="content" hit={hit} />
       </p>
-    </article>
+    </div>
   );
 };
 
 export const SearchPage = (props) => (
   <InstantSearch
+    css={css`
+      min-height: 100%;
+    `}
     searchClient={searchClient}
     indexName="uxp-photoshop"
     searchState={props.searchState}
@@ -46,19 +53,65 @@ export const SearchPage = (props) => (
     onSearchStateChange={props.onSearchStateChange}>
     <Configure attributesToSnippet={['description:10']} snippetEllipsisText="…" removeWordsIfNoResults="allOptional" />
     <SearchHeader />
-    <SearchStats />
-    <SearchResults />
-    <SearchFilters />
-    <SearchFooter />
+    <div className="search-results-main">
+      <SearchIndexes />
+      <hr className="horizontal-line" />
+      <SearchStats />
+      <div className="search-results">
+        <SearchResults />
+        <hr className="vertical-line" />
+        <SearchFilters />
+      </div>
+      <SearchFooter />
+    </div>
   </InstantSearch>
 );
 
 const SearchHeader = () => (
   <header className="search-header ">
     <div className="search-header-inner">
-      <SearchBox className="search-header-searchBox" />
+      <SearchBox />
+      <HitsPerPage
+        items={[
+          {
+            label: '16 hits per page',
+            value: 16
+          },
+          {
+            label: '32 hits per page',
+            value: 32
+          },
+          {
+            label: '64 hits per page',
+            value: 64
+          }
+        ]}
+        defaultRefinement={16}
+      />
     </div>
   </header>
+);
+
+const SearchIndexes = () => (
+  <section className="search-main-indexes">
+    <Tabs
+      css={css`
+        padding-bottom: 0;
+        margin-top: 0;
+      `}>
+      <TabsItem>
+        <TabsItemLabel>
+          <strong>Photoshop</strong>
+        </TabsItemLabel>
+      </TabsItem>
+      <TabsItem>
+        <TabsItemLabel>Illustrator</TabsItemLabel>
+      </TabsItem>
+      <TabsItem>
+        <TabsItemLabel>InDesign</TabsItemLabel>
+      </TabsItem>
+    </Tabs>
+  </section>
 );
 
 const SearchStats = () => (
@@ -69,24 +122,6 @@ const SearchStats = () => (
 
 const SearchResults = () => (
   <section className="search-results">
-    <HitsPerPage
-      className="container-option"
-      items={[
-        {
-          label: '16 hits per page',
-          value: 16
-        },
-        {
-          label: '32 hits per page',
-          value: 32
-        },
-        {
-          label: '64 hits per page',
-          value: 64
-        }
-      ]}
-      defaultRefinement={16}
-    />
     <Hits hitComponent={Hit} />
     <NoResults />
   </section>
@@ -94,8 +129,8 @@ const SearchResults = () => (
 
 const SearchFilters = () => (
   <aside className="search-filters">
-    <ClearRefinements translations={{ reset: 'Clear all filters' }} />
     <Panel header="Filters">
+      <ClearRefinements translations={{ reset: 'Clear all filters' }} />
       <RefinementList attribute="keywords" />
     </Panel>
   </aside>
