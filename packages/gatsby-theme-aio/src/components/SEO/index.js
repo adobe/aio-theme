342d971197d39748cd3fa077017e96524319d594
timkim <timk@adobe.com>
2021-12-03 11:39:48 -0800

    fix: run format

--- packages/gatsby-theme-aio/src/components/SEO/index.js
+++ packages/gatsby-theme-aio/src/components/SEO/index.js
@@ -23,7 +23,7 @@ const SEO = ({ title, description, keywords }) => (
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     <link rel="icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
-    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV &&
+    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV && (
       <script type="text/javascript">{`
         window.marketingtech = {
           'adobe': {
@@ -37,10 +37,10 @@ const SEO = ({ title, description, keywords }) => (
           }
         };
       `}</script>
-    }
-    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV &&
+    )}
+    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV && (
       <script src="https://www.adobe.com/marketingtech/main.min.js"></script>
-    }
+    )}
   </Helmet>
 );
 
