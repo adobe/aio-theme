d17a592380807f2e8e370449030d1dc1eba19bd4
Simon Wex <simon@simonwex.com>
2022-01-05 11:09:15 -0800

    Cleaning up logic to include analytics bootstrap script (#642)
    * Cleaning up logic to include analytics bootstrap script
    
    * Wrapping tags in react fragment–syntax error

--- packages/gatsby-theme-aio/src/components/SEO/index.js
+++ packages/gatsby-theme-aio/src/components/SEO/index.js
@@ -23,23 +23,24 @@ const SEO = ({ title, description, keywords }) => (
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     <link rel="icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="https://www.adobe.com/favicon.ico" type="image/x-icon" />
-    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV && (
-      <script type="text/javascript">{`
-        window.marketingtech = {
-          'adobe': {
-            'launch': {
-              'property': 'global',
-              'environment': '${process.env.GATSBY_ADOBE_ANALYTICS_ENV}'
-            },
-            'analytics': {
-              'additionalAccounts': '${process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE}' 
+    {process.env.GATSBY_ADOBE_ANALYTICS_ENV && (
+      <React.Fragment>
+        <script type="text/javascript">{`
+          window.marketingtech = {
+            'adobe': {
+              'launch': {
+                'property': 'global',
+                'environment': '${process.env.GATSBY_ADOBE_ANALYTICS_ENV || 'dev'}'
+              },
+              'analytics': {
+                'additionalAccounts': '${process.env.GATSBY_ADDITIONAL_ADOBE_ANALYTICS_ACCOUNTS || ''}'
+              }
             }
-          }
-        };
-      `}</script>
-    )}
-    {process.env.GATSBY_ADOBE_ANALYTICS_REPORT_SUITE && process.env.GATSBY_ADOBE_ANALYTICS_ENV && (
-      <script src="https://www.adobe.com/marketingtech/main.min.js"></script>
+          };
+        `}</script>
+
+        <script src="https://www.adobe.com/marketingtech/main.min.js"></script>
+      </React.Fragment>
     )}
   </Helmet>
 );
