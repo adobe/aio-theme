cdb0504b56ce85bb781aa3a9d1bcf2947c1395e7
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2022-07-11 02:20:06 -0700

    feat: communicate query string parameters to parent when search is iframed

--- packages/gatsby-theme-aio/src/components/Search/index.js
+++ packages/gatsby-theme-aio/src/components/Search/index.js
@@ -174,6 +174,8 @@ const Search = ({ algolia, searchIndex, indexAll, showSearch, setShowSearch, sea
       setQueryStringParameter(SEARCH_PARAMS.keywords, selectedKeywords);
       setQueryStringParameter(SEARCH_PARAMS.index, selectedIndex);
 
+      isIFramed ? parent.postMessage(JSON.stringify({'query':searchQuery, 'keywords':selectedKeywords, 'index':selectedIndex}), window.location.pathname) : '';
+
       positionSelectedTabIndicator();
       setShowSearchResults(true);
 
