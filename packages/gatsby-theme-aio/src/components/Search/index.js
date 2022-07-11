2c654ab9626b14593f3f72ebb430e650da9feedf
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2022-07-11 16:16:06 -0700

    fix: context switching for dev, stage, and prod

--- packages/gatsby-theme-aio/src/components/Search/index.js
+++ packages/gatsby-theme-aio/src/components/Search/index.js
@@ -174,7 +174,7 @@ const Search = ({ algolia, searchIndex, indexAll, showSearch, setShowSearch, sea
       setQueryStringParameter(SEARCH_PARAMS.keywords, selectedKeywords);
       setQueryStringParameter(SEARCH_PARAMS.index, selectedIndex);
 
-      isIFramed ? parent.postMessage(JSON.stringify({'query':searchQuery, 'keywords':selectedKeywords, 'index':selectedIndex}), window.location.origin + window.location.pathname) : '';
+      isIFramed ? parent.postMessage(JSON.stringify({ 'query': searchQuery, 'keywords': selectedKeywords, 'index': selectedIndex }), window.location.hostname === 'localhost' ? 'http://localhost:3000' : window.location.hostname === 'developer.adobe.com' ? 'https://developer.adobe.com' : 'https://developer-stage.adobe.com') : '';
 
       positionSelectedTabIndicator();
       setShowSearchResults(true);
