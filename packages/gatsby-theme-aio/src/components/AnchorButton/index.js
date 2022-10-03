48e6976d4a2676ade141665ac5137bdad8150fa7
BaskarMitrah <mit44186@adobe.com>
2022-10-03 19:58:15 +0530

    Fixed the utils wrong file path issue

--- packages/gatsby-theme-aio/src/components/AnchorButton/index.js
+++ packages/gatsby-theme-aio/src/components/AnchorButton/index.js
@@ -12,7 +12,7 @@
 
 import React from 'react';
 import { GatsbyLink } from '../GatsbyLink';
-import { getExternalLinkProps, isExternalLink, gdocsRelativeLinkFix } from '../utils';
+import { getExternalLinkProps, isExternalLink, gdocsRelativeLinkFix } from '../../utils';
 import PropTypes from 'prop-types';
 import { Button } from '../Button';
 import '@spectrum-css/button';
