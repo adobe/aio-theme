8f4df6767209d73f276b9f2338978fd999ebeb4c
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2023-02-16 12:48:47 -0800

    Fix search keyboard accessibility with reposition in layout (#1386)
    * fix: search accessibility with new position in layout

--- packages/gatsby-theme-aio/src/components/GlobalHeader/index.js
+++ packages/gatsby-theme-aio/src/components/GlobalHeader/index.js
@@ -860,6 +860,7 @@ const GlobalHeader = ({
                 }}
                 aria-label={showSearch ? "Close Search" : "Search"}
                 isQuiet
+                tabIndex="0"
                 css={css`
                   margin-right: var(--spectrum-global-dimension-size-200);
 
@@ -879,6 +880,7 @@ const GlobalHeader = ({
               variant="primary"
               href="/console"
               id={"consoleId"}
+              tabIndex="0"
               css={css`
                 @media screen and (max-width: ${MOBILE_SCREEN_WIDTH}) {
                   display: none;
