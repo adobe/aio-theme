105b72a28d91282e2ffa7cbd8faf7e800d3cbd95
louisachu <louisa@ensemble.com>
2023-05-09 09:15:49 -0700

    update css

--- packages/gatsby-theme-aio/src/components/List/index.js
+++ packages/gatsby-theme-aio/src/components/List/index.js
@@ -16,14 +16,18 @@ import PropTypes from 'prop-types';
 import '@spectrum-css/typography';
 import {css} from "@emotion/react";
 
-const List = ({ children, elementType = 'ul', className, ...props }) => {
+const List = ({ children, elementType = 'ul', css: cssOverrides, className, ...props }) => {
   const Element = elementType;
-
+  const listOverrides = `
+  margin-top: var(--spectrum-global-dimension-size-150);
+`;
   return (
     <Element {...props} className={classNames(className, 'spectrum-Body spectrum-Body--sizeM')}
-     css={css`
-      margin-top: var(--spectrum-global-dimension-size-150);
-      `}>
+      css={css`
+      ${listOverrides}
+      ${cssOverrides}
+      
+    `}>
       {children}
     </Element>
   );
