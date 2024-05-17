40bc677ff6d7c1b7f61d1d69cd821d96d9be00aa
BaskarMitrah <mit44186@adobe.com>
2024-05-17 17:49:47 +0530

    build : fixed the design issues and removed the unwanted code

--- packages/gatsby-theme-aio/src/components/GetCredential/RequestAccess/RequestAccess.js
+++ packages/gatsby-theme-aio/src/components/GetCredential/RequestAccess/RequestAccess.js
@@ -1,12 +1,15 @@
-import React from "react";
-import { RestrictedAccess } from "./RestrictedAccessFields";
-import { css } from "@emotion/react";
-import { MAX_MOBILE_WIDTH } from "../FormFields";
-import "@spectrum-css/contextualhelp/dist/index-vars.css";
-import {RequestAccessSide} from './RequestAccessSide'
+import React, { useContext } from 'react';
+import { RestrictedAccess } from './RestrictedAccessFields';
+import { css } from '@emotion/react';
+import { MAX_MOBILE_WIDTH } from '../FormFields';
+import '@spectrum-css/contextualhelp/dist/index-vars.css';
+import { RequestAccessSide } from './RequestAccessSide';
+import GetCredentialContext from '../GetCredentialContext';
 
-const RequestAccess = ({ allProps }) => {
-  const requestAccess = allProps?.[RequestAccess];
+const RequestAccess = () => {
+  const { getCredentialData } = useContext(GetCredentialContext);
+
+  const requestAccess = getCredentialData?.[RequestAccess];
   let side, restrictedAccess;
 
   if (Array.isArray(requestAccess?.children)) {
@@ -29,27 +32,22 @@ const RequestAccess = ({ allProps }) => {
           display: flex;
           flex-direction: column;
           gap: 48px;
-        `}
-      >
+        `}>
         <div
           css={css`
             display: flex;
             flex-direction: column;
             gap: 16px;
-          `}
-        >
+          `}>
           {requestAccess?.title && (
-            <h1 className="spectrum-Heading spectrum-Heading--sizeXL">
-              {requestAccess?.title}
-            </h1>
+            <h1 className="spectrum-Heading spectrum-Heading--sizeXL">{requestAccess?.title}</h1>
           )}
           {requestAccess?.paragraph && (
             <p
               className="spectrum-Body spectrum-Body--sizeL"
               css={css`
                 color: #222222;
-              `}
-            >
+              `}>
               {requestAccess?.paragraph}
             </p>
           )}
@@ -62,11 +60,8 @@ const RequestAccess = ({ allProps }) => {
             @media screen and (max-width: ${MAX_MOBILE_WIDTH}) {
               flex-direction: column;
             }
-          `}
-        >
-          {restrictedAccess && (
-            <RestrictedAccess restrictedAccess={restrictedAccess} />
-          )}
+          `}>
+          {restrictedAccess && <RestrictedAccess restrictedAccess={restrictedAccess} />}
           {side && <RequestAccessSide side={side} />}
         </div>
       </div>
