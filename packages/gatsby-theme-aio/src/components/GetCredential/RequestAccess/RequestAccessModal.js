7cdcb0c30f272b94cbffd656fd0ec35f051dfd46
Manimaran Kottursamy <mit80447@adobe.com>
2024-06-28 15:48:50 +0530

    fix: Upon requesting access, the request pending status does not show up automatically, I have to refresh the page or the request access button keeps showing. If I fill out the form and change the org, the form fields reset. Only the dev terms acceptance should reset. Rest everything should stay the same.

--- packages/gatsby-theme-aio/src/components/GetCredential/RequestAccess/RequestAccessModal.js
+++ packages/gatsby-theme-aio/src/components/GetCredential/RequestAccess/RequestAccessModal.js
@@ -1,51 +1,77 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import "@spectrum-css/contextualhelp/dist/index-vars.css";
 import {
   Dialog,
   ProgressCircle,
 } from "@adobe/react-spectrum";
+import GetCredentialContext from "../GetCredentialContext";
 
 const INITIAL_IFRAME_HEIGHT = 420;
 
-const RequestAccessModal = ({accessPlatformAppId, close}) => {
+const RequestAccessModal = ({ accessPlatformAppId, close }) => {
   const [targetUrl, setTargetUrl] = useState('');
   const [loading, setLoading] = useState(true);
-  const [iframeHeight, setIframeHeight] =  useState(INITIAL_IFRAME_HEIGHT);
+  const [iframeHeight, setIframeHeight] = useState(INITIAL_IFRAME_HEIGHT);
+  const { setTemplate, template, selectedOrganization } = useContext(GetCredentialContext);
 
   useEffect(() => {
     const setTargetUrlHelper = async () => {
-        const { userId } = await window.adobeIMS.getProfile();
-        const acrsHostPrefix = window.adobeIMS.adobeIdData.environment === 'prod' ? '' : 'stage.';
-        setTargetUrl(`https://${acrsHostPrefix}acrs.adobe.com/requestAccess?flow=frame&colorScheme=light&applicationId=UDPWeb1&appId=${accessPlatformAppId}&userId=${userId}&accessRequestType=apiAccess`);
+      const { userId } = await window.adobeIMS.getProfile();
+      const acrsHostPrefix = window.adobeIMS.adobeIdData.environment === 'prod' ? '' : 'stage.';
+      setTargetUrl(`https://${acrsHostPrefix}acrs.adobe.com/requestAccess?flow=frame&colorScheme=light&applicationId=UDPWeb1&appId=${accessPlatformAppId}&userId=${userId}&accessRequestType=apiAccess`);
     };
     setTargetUrlHelper();
-}, [setTargetUrl, accessPlatformAppId]);
+  }, [setTargetUrl, accessPlatformAppId]);
 
-const handleIframeMessage = useCallback(
+  const handleIframeMessage = useCallback(
     (event) => {
-        if (!event.isTrusted || (typeof event.data !== 'string' && !(event.data instanceof String))) {
-            return;
-        }
-        const eventData = JSON.parse(event.data);
+      if (!event.isTrusted || (typeof event.data !== 'string' && !(event.data instanceof String))) {
+        return;
+      }
+      const eventData = JSON.parse(event.data);
 
-        if (!eventData || eventData.app !== 'acrs-request-access' || eventData.type !== 'System') {
-            return;
-        }
+      if (!eventData || eventData.app !== 'acrs-request-access' || eventData.type !== 'System') {
+        return;
+      }
 
-        switch (eventData.subType) {
-            case 'AppLoaded':
-                setLoading(false);
-                break;
-            case 'Resize':
-                setIframeHeight(eventData.data.height);
-                break;
-            case 'Close':
-                close();
-                break;
-        }
+      switch (eventData.subType) {
+        case 'AppLoaded':
+          setLoading(false);
+          break;
+        case 'Resize':
+          setIframeHeight(eventData.data.height);
+          break;
+        case 'Close':
+          close();
+          fetchTemplate();
+          break;
+      }
     },
     [close, setLoading, setIframeHeight],
   );
+
+  const fetchTemplate = async () => {
+    try {
+      const url = `/templates/${template.id}`
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${window.adobeIMS.getTokenFromStorage().token}`,
+          "x-api-key": window.adobeIMS.adobeIdData.client_id,
+          "x-org-id": selectedOrganization.code
+        }
+      });
+
+      if (!response.ok) {
+        console.error('Template not found. Please check template id');
+        return;
+      }
+      setTemplate(await response.json());
+    }
+    catch (error) {
+      console.error('Error fetching template:', error);
+    }
+  }
 
   useEffect(() => {
     window.addEventListener('message', handleIframeMessage);
