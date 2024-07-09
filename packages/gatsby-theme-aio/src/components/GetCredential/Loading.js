fe6416abbfd70408c60496e1ce2d1e143b23bb7a
manimitrah <168568579+manimitrah@users.noreply.github.com>
2024-07-09 21:28:54 +0530

    fix: added loading text to the loader (#1595)

--- packages/gatsby-theme-aio/src/components/GetCredential/Loading.js
+++ packages/gatsby-theme-aio/src/components/GetCredential/Loading.js
@@ -5,7 +5,8 @@ import { ProgressCircle } from '@adobe/react-spectrum';
 const Loading = ({
   credentials,
   downloadStatus,
-  isCreateCredential
+  isCreateCredential,
+  initialLoading
 }) => {
   const divRef = useRef(null);
   useEffect(() => {
@@ -29,6 +30,16 @@ const Loading = ({
           gap:10px;
         `}>
         <ProgressCircle size="L" aria-label="Loading…" isIndeterminate />
+        {initialLoading &&
+          <div ref={divRef}
+            css={css`
+            font-style: italic;
+            font-family: 'adobe-clean';
+            color: var(--spectrum-dialog-confirm-description-text-color, var(--spectrum-global-color-gray-600));
+          `}
+          >
+            {"Loading..."}
+          </div>}
         <div ref={divRef}
           css={css`
             font-style: italic;
