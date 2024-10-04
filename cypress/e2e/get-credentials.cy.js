43972abff7486543001ed6dd568dca765b931deb
BaskarMitrah <mit44186@adobe.com>
2024-10-04 12:41:21 +0530

    Fix : Generate token issue

--- cypress/e2e/get-credentials.cy.js
+++ cypress/e2e/get-credentials.cy.js
@@ -93,14 +93,18 @@ function checkAPIKey() {
 
 function checkOAuthS2S() {
 
-    // Check if the collapse open button is present and click it if it is
-    cy.get('body').then(($body) => {
-      if ($body.find('[data-cy="collapse-open"]').length) {
-        cy.get('[data-cy="collapse-open"]').click();
-      }
-    });
+  // Check if the collapse open button is present and click it if it is
+  cy.get('body').then(($body) => {
+    if ($body.find('[data-cy="collapse-open"]').length) {
+      cy.get('[data-cy="collapse-open"]').click();
+    }
+  });
 
-  cy.get('div[data-cy="generate-token"]').should('be.visible');
+  cy.get('body').then(($body) => {
+    if ($body.find('[data-cy="generate-token"]').length) {
+      cy.get('[data-cy="generate-token"]').should('be.visible');
+    }
+  });
   // cy.get('button[data-cy="copy-token"]').should('exist');
   cy.get('[data-cy="credentialName-link"]').should('exist');
   cy.get('[data-cy="ClientId-copyIcon"]').should('exist');
